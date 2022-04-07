5231441bc56999ecbffe1de6c394edd710c08cda
guillaume <guillaume.belanger27@gmail.com>
2022-04-07 15:57:42 -0400

    - Improves error handling to simply print the error messages instead of the whole Python Traceback errors

--- magma_access_gateway_installer/agw_installation_errors.py
+++ magma_access_gateway_installer/agw_installation_errors.py
@@ -10,29 +10,30 @@ logger = logging.getLogger("magma_access_gateway_installer")
 class AGWInstallationError(Exception):
     """Base class for Magma AGW installation exceptions."""
 
-    pass
+    def __init__(self, message):
+        self._message = f"ERROR: {message} Exiting installation!"
+        logger.info(self._message)
+
+    @property
+    def message(self):
+        return self._message
 
 
 class InvalidUserError(AGWInstallationError):
-    """Exception raised when installation has been started by user different than root."""
+    """Exception raised when installation has been started by user different from root."""
 
-    ERROR_MESSAGE = (
-        "Invalid user! \n"
-        "Magma AGW installation should be performed by the root user! Exiting..."
-    )
+    ERROR_MESSAGE = "Invalid user. Installation should be performed by the root user."
 
     def __init__(self):
-        logger.error(self.ERROR_MESSAGE)
         super().__init__(self.ERROR_MESSAGE)
 
 
 class UnsupportedOSError(AGWInstallationError):
     """Exception raised when installation has been started on an OS other than Ubuntu."""
 
-    ERROR_MESSAGE = "Invalid OS! \n Magma AGW can only be installed on Ubuntu! Exiting..."
+    ERROR_MESSAGE = "Invalid OS. Only Ubuntu 20.04 is supported."
 
     def __init__(self):
-        logger.error(self.ERROR_MESSAGE)
         super().__init__(self.ERROR_MESSAGE)
 
 
@@ -40,10 +41,9 @@ class InvalidNumberOfInterfacesError(AGWInstallationError):
     """Exception raised if number of available network interfaces is different from expected."""
 
     ERROR_MESSAGE = (
-        "Invalid number of network interfaces! \n"
-        "Magma AGW needs at least two network interfaces available - SGi and S1! Exiting..."
+        "Invalid number of network interfaces. "
+        "Installation requires at least two network interfaces available."
     )
 
     def __init__(self):
-        logger.error(self.ERROR_MESSAGE)
         super().__init__(self.ERROR_MESSAGE)
